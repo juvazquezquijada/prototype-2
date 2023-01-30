ac0c767212b14d0382888a7d6615dd538eab25bb
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-01-30 14:43:59 -0800

    Spacebar script

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space));
         {
-            Instantiate(projectilePrefab, transform.postion, projectilePrefab.transform.rotation);
+           Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }   
 }
