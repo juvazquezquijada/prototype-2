e744bb37c9e48e95e9ef6443a9bc98f7f3d0a966
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-02-01 10:46:51 -0800

    fixed spacebar script and added destroy out of bounds scripts

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 15;
-
     public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
@@ -31,7 +30,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space));
+        if (Input.GetKeyDown(KeyCode.Space))
         {
            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
