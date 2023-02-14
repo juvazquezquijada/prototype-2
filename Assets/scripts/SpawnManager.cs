d66d16f4cbc1463b70f2f1926030a7981b057d22
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-02-13 20:56:51 -0800

    added box colliders to animal prefabs and added detect collisons script
    also made changes to spawn manager script

--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -7,16 +7,19 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;
     private float spawnRangeX = 20;
     private float spawnRangeZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S)) 
+        
         {
             
         }     
@@ -28,6 +31,6 @@ public class SpawnManager : MonoBehaviour
           int animalIndex = Random.Range(0, animalPrefabs.Length);
           Vector3 spawnPos = new Vector3(Random.Range(-20, 20), 0 ,spawnRangeZ);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[0].transform.rotation);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
      }
 }
