dc42c0f2dc2c4ded33ee90c772789d0056bd5c04
juvazquezquijada <121971788+juvazquezquijada@users.noreply.github.com>
2023-02-13 21:07:28 -0800

    changed space button input to left mouse button
    completed unit 2 part 4

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonDown(0))
         {
            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
